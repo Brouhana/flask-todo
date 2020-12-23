2df7fd04fd7eb377d61c74859f802ff8dad7300b
Brennan Rouhana <brouhana@Brennans-MacBook-Pro-2.local>
2020-12-23 01:26:28 -0600

    Changed templates, added routes

--- app.py
+++ app.py
@@ -2,15 +2,18 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask.globals import request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(300), nullable=False)
     is_important = db.Column(db.Boolean(False), nullable=True)
+    is_follow_up = db.Column(db.Boolean(False), nullable=True)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
@@ -70,19 +73,43 @@ def edit(id):
 
 @app.route('/important/<int:id>')
 def mark_important(id):
-        
-    task = Todo.query.get_or_404(id)
+    task_to_mark = Todo.query.get_or_404(id)
     
     try:
-        if task.is_important:
-            task.is_important = 0
+        if task_to_mark.is_important:
+            task_to_mark.is_important = 0
         else:
-            task.is_important = 1
+            task_to_mark.is_important = 1
     except:
         return render_template('failure.html')
 
     db.session.commit()
     return redirect('/')
     
+@app.route('/follow-up/<int:id>')
+def follow_up(id):
+    task_to_follow = Todo.query.get_or_404(id)
+    
+    try:
+        if task_to_follow.is_follow_up:
+            task_to_follow.is_follow_up = 0
+        else:
+            task_to_follow.is_follow_up = 1
+    except:
+        return render_template('failure.html')
+    
+    db.session.commit()
+    return redirect('/')
+
+@app.route('/tag/important', methods=['GET'])
+def important_index():
+    tasks = Todo.query.filter_by(is_important='1').all()
+    return render_template('important.html', tasks=tasks)
+
+@app.route('/tag/follow-up', methods=['GET'])
+def follow_up_index():
+    tasks = Todo.query.filter_by(is_follow_up='1').all()
+    return render_template('follow-up.html', tasks=tasks)
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
